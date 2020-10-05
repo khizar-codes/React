0fbebd3c227b8cc656322f0450665e8685ddf105
Muhammad Khizar <mkhizar2001@gmail.com>
2020-10-04 19:05:24 -0500

    React todo list app
    Cool web stuff!

---
+++ todoListApp/src/todosData.js
@@ -0,0 +1,31 @@
+const todosData = [
+
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Fight corona",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Box Mayweather",
+        completed: true
+    },
+    {
+        id: 4,
+        text: "Create react app",
+        completed: false
+    },
+    {
+        id: 5,
+        text: "Eat a computer",
+        completed: true
+    }
+
+]
+
+export default todosData
